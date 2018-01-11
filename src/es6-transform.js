c850c9196e14762cdc44504a55727b9a3c2fd9ca
Sander Melnikov <sandydoo@users.noreply.github.com>
2018-01-11 16:55:55 +0300

    Pre-merge cleanup

--- src/es6-transform.js
+++ src/es6-transform.js
@@ -7,9 +7,8 @@ module.exports = function(tree, babelOptions, options) {
   babelOptions = babelOptions || {};
   options = options || {};
   const defaultBabelOptions = {
-    getModuleId: function (moduleName) {
-      const moduleKey = moduleName + '.js';
-      const assetOptions = options[moduleKey];
+    getModuleId: function(moduleName) {
+      const assetOptions = options[`${moduleName}.js`];
       if (assetOptions) {
         return assetOptions.as;
       } else {
